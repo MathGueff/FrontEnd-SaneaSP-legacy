17b4c324b997f919aba0b824ba25f5024ff35f86
Davy Ribeiro <davy.oliv.ribeiro@gmail.com>
2025-09-08 17:50:26 -0300

    fix: tentando fazer o map funcionanr

--- src/app/features/autenticacao/pages/form-cadastro/form-cadastro.component.ts
+++ src/app/features/autenticacao/pages/form-cadastro/form-cadastro.component.ts
@@ -228,20 +228,27 @@ export class FormCadastroComponent implements OnInit {
 
   ngOnInit() {
     //
+    this.coordanates = {
+      lat: -23.5062,
+      lon: -47.4559
+    }
+
     this.formCadastro.valueChanges.subscribe((form) => {
       if (form.logradouro || form.cep) {
         let street = `${form.logradouro}`;
         let number = `${form.numero}`;
         let city = `Sorocaba`
 
-        this.geocodingService.geolocation(`${street}, ${number}, ${city}, São Paulo, Brazil`).subscribe({
-          next:(result)=>{
-            this.coordanates = result;
-          },
-          error:(err)=>{
-            this.sweetAlertService.showMessage('Erro ao procurar a coordenada: ' + err)
-          }
-        })
+
+        
+        // this.geocodingService.geolocation(`${street}, ${number}, ${city}, São Paulo, Brazil`).subscribe({
+        //   next:(result)=>{
+        //     this.coordanates = result;
+        //   },
+        //   error:(err)=>{
+        //     this.sweetAlertService.showMessage('Erro ao procurar a coordenada: ' + err)
+        //   }
+        // })
       }
     });
     //VIACEP
--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -6,30 +6,47 @@ import { AfterViewInit, Component, Inject, Input, PLATFORM_ID } from "@angular/c
   imports: [],
   template: `<div id="map"></div>`,
   styleUrl: "./map.component.css",
-  standalone:true,
-  host:{ ngSkipHydration: 'true' }
+  standalone: true,
+  host: { ngSkipHydration: 'true' }
 })
 export class MapComponent implements AfterViewInit {
   private isBrowser!: boolean;
-  @Input() lat ?: number;
-  @Input() lon ?: number;
-  constructor(@Inject(PLATFORM_ID) private platformId: Object){
+  @Input() lat: number = 23.55052;
+  @Input() lon: number = -46.633308;
+ 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     this.isBrowser = isPlatformBrowser(this.platformId);
   }
-   async ngAfterViewInit() {
-    if(!this.isBrowser){
-      return;
-    }
-    const L = await import('leaflet');
-    let map = L.map('map')
-    
-    if(this.lat && this.lon){
-      map.setView([this.lat,this.lon],13);
-    }
+  async ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Importar dinamicamente apenas no browser
+      const L = await import('leaflet');
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        (L.Icon.Default as any).mergeOptions({
+        iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+        iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+        shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+      });
+
+      // Corrigir problema de ícones do Leaflet no Angular
+      (L.Icon.Default as any).mergeOptions({
+        iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+        iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+        shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+      });
+
+      const container = L.DomUtil.get('map');
+      if (container != null) {
+        (container as any)._leaflet_id = null; // força reset do container
+      }
+      const map = L.map('map').setView([this.lat, this.lon], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+      }).addTo(map);
+
+      L.marker([this.lat, this.lon]).addTo(map);
+    }
   }
 
 }
