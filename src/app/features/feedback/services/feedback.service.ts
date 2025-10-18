308a54870d9ad80bfd7315f52115b2f102063016
Ryan <ryanjobs234@gmail.com>
2025-10-18 09:17:44 -0300

    fix(feedback): correção dos métodos, agora envia o token do usuário logado

--- src/app/features/feedback/services/feedback.service.ts
+++ src/app/features/feedback/services/feedback.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from "environments/environment";
 import { AuthService } from "@core/services/auth.service";
 import { Observable } from "rxjs";
@@ -15,8 +15,19 @@ export class FeedbackService {
 
     constructor(private httpClient: HttpClient,) { }
 
+    private currentUser = this.authService.currentUser$;
+
     public postFeedback(newFeedback: ICreateFeedback): Observable<IFeedback> {
-        return this.httpClient.post<IFeedback>(`${this.urlApi}/create-feedback`, newFeedback);
+        return this.httpClient.post<IFeedback>(`${this.urlApi}/create-feedback`, newFeedback, { headers: this.getAuthHeaders() });
+    }
+
+    private getAuthHeaders(): HttpHeaders {
+        const token = this.authService.getAuthToken();
+        let headers = new HttpHeaders();
+        if (token) {
+            headers = headers.set('Authorization', token);
+        }
+        return headers;
     }
 
     private feedbacks: IFeedback[] = [];
@@ -24,8 +35,12 @@ export class FeedbackService {
     public getAllFeedBacks(): IFeedback[] {
         return this.feedbacks;
     }
-
+    /*
     public putFeedback(feedback: IFeedback): Observable<IFeedback> {
         return this.httpClient.put<IFeedback>(`${this.urlApi}/${feedback.id}`, feedback);
+    }
+    */
+    public deleteFeedback(id: number): Observable<void> {
+        return this.httpClient.delete<void>(`${this.urlApi}/${id}`, { headers: this.getAuthHeaders() });
     }
 }
